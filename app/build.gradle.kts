48e7ce9b67dd77afa550932276a6680b02957144
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-04-12 15:09:47 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,11 @@ plugins {
 }
 
 android {
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     namespace = "com.rafa.musicas"
     compileSdk = 34
 
