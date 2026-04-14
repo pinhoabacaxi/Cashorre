8a3b52168e350f4c83beabcebd7dbb6fa78eb1a0
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-04-14 00:58:44 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,9 @@ plugins {
 }
 
 android {
+    buildFeatures {
+    compose = true
+}
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -26,8 +29,13 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
+        kotlinCompilerExtensionVersion = "1.5.14" 
     }
+
+    composeOptions {
+    kotlinCompilerExtensionVersion = "1.5.8"
+    }
+        
 
     kotlinOptions {
         jvmTarget = "17"
@@ -44,6 +52,10 @@ dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2024.06.00")
     implementation(composeBom)
     androidTestImplementation(composeBom)
+
+    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.activity:activity-compose:1.8.2")
 
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.1")
