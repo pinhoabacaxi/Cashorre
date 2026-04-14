1e34c1f131eac3178746c81fc6fec04f996f0c9e
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-04-14 04:14:44 -0300

    Update Models.kt

--- app/src/main/java/com/rafa/musicas/data/Models.kt
+++ app/src/main/java/com/rafa/musicas/data/Models.kt
@@ -1,18 +1,3 @@
 package com.rafa.musicas.data
 
 import kotlinx.serialization.Serializable
-
-@Serializable
-data class TrackEntry(
-    val fileName: String,
-    val displayName: String = fileName,
-    val title: String? = null,
-    val artist: String? = null,
-    val durationMs: Long? = null
-)
-
-@Serializable
-data class PlaylistIndex(
-    val playlistName: String,
-    val tracks: List<TrackEntry>
-)
