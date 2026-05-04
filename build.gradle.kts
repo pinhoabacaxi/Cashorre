304d4ce61fc5b99393ec4aaf909e4ea0adef965b
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-04 20:03:39 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
-// build.gradle.kts raiz
 plugins {
-    id("com.android.application") version "8.8.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10" apply false
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
