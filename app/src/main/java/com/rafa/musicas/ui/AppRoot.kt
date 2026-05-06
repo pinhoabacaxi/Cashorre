240dd796b35beabc718414f4eac2d99301291d07
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-06 20:32:02 -0300

    Update AppRoot.kt

--- app/src/main/java/com/rafa/musicas/ui/AppRoot.kt
+++ app/src/main/java/com/rafa/musicas/ui/AppRoot.kt
@@ -1,10 +1,10 @@
 package com.rafa.musicas.ui
 
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.media3.common.util.UnstableApi
 import android.content.Intent
+import androidx.annotation.OptIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
@@ -12,12 +12,12 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
 import androidx.media3.common.util.UnstableApi
 import androidx.navigation.NavType
-import androidx.navigation.compose.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.rafa.musicas.data.PlaylistStore
-import com.rafa.musicas.player.PlaybackService
 
-@androidx.annotation.OptIn(UnstableApi::class)
+@OptIn(UnstableApi::class)
 @kotlin.OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppRoot(store: PlaylistStore) {
