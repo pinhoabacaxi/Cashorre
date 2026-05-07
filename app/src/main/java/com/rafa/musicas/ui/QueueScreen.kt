47b93f482324a2c94eb33999702f536318f1e654
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-07 01:44:36 -0300

    Update QueueScreen.kt

--- app/src/main/java/com/rafa/musicas/ui/QueueScreen.kt
+++ app/src/main/java/com/rafa/musicas/ui/QueueScreen.kt
@@ -10,7 +10,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.Icons*
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.KeyboardArrowDown
