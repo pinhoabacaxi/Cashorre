bfb778a4fad2c78c31aa4b6afd356eb2aa038868
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-07 00:21:55 -0300

    Update ArtworkBox.kt

--- app/src/main/java/com/rafa/musicas/ui/ArtworkBox.kt
+++ app/src/main/java/com/rafa/musicas/ui/ArtworkBox.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import androidx.compose.foundation.layout.matchParentSize
 
 @Composable
 fun ArtworkBox(
