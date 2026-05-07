c47d4696bbcda12ac08738ec7900cc0c0041507a
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-06 21:18:39 -0300

    Create LibraryScreen.kt

---
+++ app/src/main/java/com/rafa/musicas/ui/LibraryScreen.kt
@@ -0,0 +1,49 @@
+package com.rafa.musicas.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.LibraryMusic
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.rafa.musicas.data.db.MusicEntity
+import com.rafa.musicas.data.toMediaItem
+import com.rafa.musicas.player.PlayerManager
+
+@Composable
+fun LibraryScreen(
+    viewModel: LibraryViewModel = viewModel()
+) {
+    val context = LocalContext.current
+    val tracks by viewModel.tracks.collectAsState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Row(
+            modifier
