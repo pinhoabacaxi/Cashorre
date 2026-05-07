580b1ffb4d6333036a677da9e23431ee5a0d0c10
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-06 23:17:11 -0300

    Update PlaybackService.kt

--- app/src/main/java/com/rafa/musicas/player/PlaybackService.kt
+++ app/src/main/java/com/rafa/musicas/player/PlaybackService.kt
@@ -2,43 +2,69 @@ package com.rafa.musicas.player
 
 import android.app.PendingIntent
 import android.content.Intent
+import androidx.annotation.OptIn
+import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
 import com.rafa.musicas.MainActivity
 
-@UnstableApi
+@OptIn(UnstableApi::class)
 class PlaybackService : MediaSessionService() {
 
     private var mediaSession: MediaSession? = null
 
     override fun onCreate() {
-    super.onCreate()
-
-    startForeground(
-        1,
-        NotificationHelper.createNotification(this)
-    )
+        super.onCreate()
 
         val player = PlayerManager.get(this)
 
-        val intent = Intent(this, MainActivity::class.java)
-        val pi = PendingIntent.getActivity(
-            this, 0, intent,
+        val sessionActivityPendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            Intent(this, MainActivity::class.java),
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
 
         mediaSession = MediaSession.Builder(this, player)
-            .setSessionActivity(pi)
+            .setSessionActivity(sessionActivityPendingIntent)
             .build()
+
+        player.addListener(
+            object : Player.Listener {
+                override fun onIsPlayingChanged(isPlaying: Boolean) {
+                    PlayerManager.saveQueue(this@PlaybackService)
+                }
+
+                override fun onMediaItemTransition(
+                    mediaItem: androidx.media3.common.MediaItem?,
+                    reason: Int
+                ) {
+                    PlayerManager.saveQueue(this@PlaybackService)
+                }
+            }
+        )
     }
 
-    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? {
+    override fun onGetSession(
+        controllerInfo: MediaSession.ControllerInfo
+    ): MediaSession? {
         return mediaSession
     }
 
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        val player = mediaSession?.player
+
+        if (player == null || !player.playWhenReady || player.mediaItemCount == 0) {
+            stopSelf()
+        }
+    }
+
     override fun onDestroy() {
-        mediaSession?.release()
+        mediaSession?.run {
+            player.release()
+            release()
+        }
         mediaSession = null
         super.onDestroy()
     }
