4371e22791fbf7dd10912dc01305016a200484c2
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-08 10:16:21 -0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven { url 'https://jitpack.io' 
+        maven { url "https://jitpack.io" 
     }
 }
 dependencyResolutionManagement {
@@ -11,9 +11,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url 'https://jitpack.io' 
+        maven { url "https://jitpack.io" 
     }
 }
 rootProject.name = "Musicas"
 include(":app")
-include (':kotlinYoutubeExtractor')
+include (":kotlinYoutubeExtractor")
+}
