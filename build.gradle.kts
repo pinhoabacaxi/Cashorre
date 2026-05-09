eb7ea15ab686889358d81469dfec786a7d197ebe
pinhoabacaxi <rafael.goncalves1424@gmail.com>
2026-05-08 22:56:45 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.25" apply false
+    id("com.android.library") version "7.4.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.5.3" apply false
+    id("com.android.application") version "7.4.2" apply false
 }
